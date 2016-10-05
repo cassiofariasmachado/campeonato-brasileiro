2c67778a7f1f0c5f016fb45ad91d0b1710b39f04
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-05 08:47:35 -0300

    fix(Jogador): transformar variavéis que representam os jogadores em objetos do tipo Jogador.

--- banco_de_dados_II-jpa_e_db4o/src/br/unisinos/bd2/campeonato/brasileiro/db4o/model/Clube.java
+++ banco_de_dados_II-jpa_e_db4o/src/br/unisinos/bd2/campeonato/brasileiro/db4o/model/Clube.java
@@ -11,42 +11,42 @@ public class Clube implements Serializable {
 	
 	private String nome;
 	
-	private Long jogador1;
+	private Jogador jogador1;
 	
-	private Long jogador2;
+	private Jogador jogador2;
 	
-	private Long jogador3;
+	private Jogador jogador3;
 	
-	private Long jogador4;
+	private Jogador jogador4;
 	
-	private Long jogador5;
+	private Jogador jogador5;
 	
-	private Long jogador6;
+	private Jogador jogador6;
 	
-	private Long jogador7;
+	private Jogador jogador7;
 	
-	private Long jogador8;
+	private Jogador jogador8;
 	
-	private Long jogador9;
+	private Jogador jogador9;
 	
-	private Long jogador10;
+	private Jogador jogador10;
 	
-	private Long jogador11;
+	private Jogador jogador11;
 	
-	private Long jogador12;
+	private Jogador jogador12;
 	
-	private Long jogador13;
+	private Jogador jogador13;
 	
-	private Long jogador14;
+	private Jogador jogador14;
 	
-	private Long jogador15;
+	private Jogador jogador15;
 	
-	private Long jogador16;
+	private Jogador jogador16;
 	
 	// Metodos
-	public Clube(Long idClube, String nome, Long jogador1, Long jogador2, Long jogador3, 
-			Long jogador4, Long jogador5, Long jogador6, Long jogador7, Long jogador8, Long jogador9, Long jogador10,
-			Long jogador11, Long jogador12, Long jogador13, Long jogador14, Long jogador15, Long jogador16){
+	public Clube(Long idClube, String nome, Jogador jogador1, Jogador jogador2, Jogador jogador3, 
+			Jogador jogador4, Jogador jogador5, Jogador jogador6, Jogador jogador7, Jogador jogador8, Jogador jogador9, Jogador jogador10,
+			Jogador jogador11, Jogador jogador12, Jogador jogador13, Jogador jogador14, Jogador jogador15, Jogador jogador16){
 		super();
 		this.idClube = idClube;
 		this.nome = nome;
@@ -67,7 +67,8 @@ public class Clube implements Serializable {
 		this.jogador15 = jogador15;
 		this.jogador16 = jogador16;
 	}
-	
+
+	// Métodos
 	public Long getIdClube() {
 		return idClube;
 	}
@@ -84,132 +85,135 @@ public class Clube implements Serializable {
 		this.nome = nome;
 	}
 
-	public Long getJogador1() {
+	public Jogador getJogador1() {
 		return jogador1;
 	}
 
-	public void setJogador1(Long jogador1) {
+	public void setJogador1(Jogador jogador1) {
 		this.jogador1 = jogador1;
 	}
 
-	public Long getJogador2() {
+	public Jogador getJogador2() {
 		return jogador2;
 	}
 
-	public void setJogador2(Long jogador2) {
+	public void setJogador2(Jogador jogador2) {
 		this.jogador2 = jogador2;
 	}
 
-	public Long getJogador3() {
+	public Jogador getJogador3() {
 		return jogador3;
 	}
 
-	public void setJogador3(Long jogador3) {
+	public void setJogador3(Jogador jogador3) {
 		this.jogador3 = jogador3;
 	}
 
-	public Long getJogador4() {
+	public Jogador getJogador4() {
 		return jogador4;
 	}
 
-	public void setJogador4(Long jogador4) {
+	public void setJogador4(Jogador jogador4) {
 		this.jogador4 = jogador4;
 	}
 
-	public Long getJogador5() {
+	public Jogador getJogador5() {
 		return jogador5;
 	}
 
-	public void setJogador5(Long jogador5) {
+	public void setJogador5(Jogador jogador5) {
 		this.jogador5 = jogador5;
 	}
 
-	public Long getJogador6() {
+	public Jogador getJogador6() {
 		return jogador6;
 	}
 
-	public void setJogador6(Long jogador6) {
+	public void setJogador6(Jogador jogador6) {
 		this.jogador6 = jogador6;
 	}
 
-	public Long getJogador7() {
+	public Jogador getJogador7() {
 		return jogador7;
 	}
 
-	public void setJogador7(Long jogador7) {
+	public void setJogador7(Jogador jogador7) {
 		this.jogador7 = jogador7;
 	}
 
-	public Long getJogador8() {
+	public Jogador getJogador8() {
 		return jogador8;
 	}
 
-	public void setJogador8(Long jogador8) {
+	public void setJogador8(Jogador jogador8) {
 		this.jogador8 = jogador8;
 	}
 
-	public Long getJogador9() {
+	public Jogador getJogador9() {
 		return jogador9;
 	}
 
-	public void setJogador9(Long jogador9) {
+	public void setJogador9(Jogador jogador9) {
 		this.jogador9 = jogador9;
 	}
 
-	public Long getJogador10() {
+	public Jogador getJogador10() {
 		return jogador10;
 	}
 
-	public void setJogador10(Long jogador10) {
+	public void setJogador10(Jogador jogador10) {
 		this.jogador10 = jogador10;
 	}
 
-	public Long getJogador11() {
+	public Jogador getJogador11() {
 		return jogador11;
 	}
 
-	public void setJogador11(Long jogador11) {
+	public void setJogador11(Jogador jogador11) {
 		this.jogador11 = jogador11;
 	}
 
-	public Long getJogador12() {
+	public Jogador getJogador12() {
 		return jogador12;
 	}
 
-	public void setJogador12(Long jogador12) {
+	public void setJogador12(Jogador jogador12) {
 		this.jogador12 = jogador12;
 	}
 
-	public Long getJogador13() {
+	public Jogador getJogador13() {
 		return jogador13;
 	}
 
-	public void setJogador13(Long jogador13) {
+	public void setJogador13(Jogador jogador13) {
 		this.jogador13 = jogador13;
 	}
 
-	public Long getJogador14() {
+	public Jogador getJogador14() {
 		return jogador14;
 	}
 
-	public void setJogador14(Long jogador14) {
+	public void setJogador14(Jogador jogador14) {
 		this.jogador14 = jogador14;
 	}
 
-	public Long getJogador15() {
+	public Jogador getJogador15() {
 		return jogador15;
 	}
 
-	public void setJogador15(Long jogador15) {
+	public void setJogador15(Jogador jogador15) {
 		this.jogador15 = jogador15;
 	}
 
-	public Long getJogador16() {
+	public Jogador getJogador16() {
 		return jogador16;
 	}
 
-	public void setJogador16(Long jogador16) {
+	public void setJogador16(Jogador jogador16) {
 		this.jogador16 = jogador16;
 	}
-	
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
